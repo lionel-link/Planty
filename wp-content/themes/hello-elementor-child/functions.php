b2f9ab2e93bcd58b5ff3af8a9a06c345a9c702dd
lionel-link <lioneladje@gmail.com>
2024-09-02 10:07:07 +0200

    responsive tablet and mobile

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -1,24 +1,52 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+if (!function_exists('chld_thm_cfg_locale_css')):
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
             $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style','hello-elementor-header-footer' ) );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
+
+if (!function_exists('child_theme_configurator_css')):
+    function child_theme_configurator_css()
+    {
+        wp_enqueue_style('chld_thm_cfg_child', trailingslashit(get_stylesheet_directory_uri()) . 'style.css', array('hello-elementor', 'hello-elementor', 'hello-elementor-theme-style', 'hello-elementor-header-footer'));
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 10);
 
 // END ENQUEUE PARENT ACTION
+
+
+// Gestion show admin link if user is logged
+
+function add_admin_link_to_nav_menu($items, $args)
+{
+    //var_dump($args->menu);
+    //var_dump($items);
+
+    if (is_user_logged_in() && $args->menu == 'navigation') {
+
+        $item = '<li><a href="' . get_admin_url() . '">Admin</a></li>';
+
+        // Convertir les éléments du menu en tableau
+        $items_array = explode('</li>', $items);
+
+        // Insérer le nouvel élément en deuxième position
+        array_splice($items_array, 1, 0, $item);
+
+        // Reconstruire les éléments du menu
+        $items = implode('</li>', $items_array);
+    }
+
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_nav_menu', 10, 2);
